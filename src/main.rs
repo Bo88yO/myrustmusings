c8ffe43792f00541e78675fed63c4898978a4146
Bo88yO <a6b7de3e@opayq.com>
2022-06-29 18:33:27 +0930

    Including comments and printing variables.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 pub fn main() {
-    println("Hello, world! ");
+    // this is a single line comment.
+
+    /* this is a multi line block comment.  these can be used within a line. */
+    let x = 5 + 90 + 5;
+    println!("Hello, world! ");
+    println!(" The value of x is {}", x);
 }
